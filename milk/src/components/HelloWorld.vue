2497a1235285a671ee4255c48e1188adf35ecb98
Yuesheng <zhuo.shenghui@mitigenomics.com>
2018-03-28 16:39:44 +0800

    change

---
+++ milk/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>nice to meet you</h1>
+    <h2>nice to meet you too</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
